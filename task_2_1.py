5dae733863a7ada3de19f9f60fb66b3bdc79227c
Olebuhoe <olebuhoe@yandex.ru>
2021-10-07 17:41:58 +0300

    Окончательные варианты

---
+++ task_2_1.py
@@ -0,0 +1,16 @@
+a = 15 * 3
+b = 15 / 3
+c = 15 // 3
+d = 15 ** 3
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(isinstance(a, int))
+print(isinstance(b, float))
+print(isinstance(c, int))
+print(isinstance(d, int))
+
+print('End')
